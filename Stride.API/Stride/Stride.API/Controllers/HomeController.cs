dac2df96b439047f6d09940bb2f2f75939ec09ac
Hozefa Mangalorewala <hozefam@gmail.com>
2025-03-02 13:38:36 +0530

    API - Initial Commit

---
+++ Stride.API/Stride/Stride.API/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Stride.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		private readonly IConfiguration _configuration;
+
+		public HomeController(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		[HttpGet("version")]
+		public IActionResult GetVersion()
+		{
+			var version = _configuration["ApiVersion"];
+			return Ok(new { version });
+		}
+	}
+}
